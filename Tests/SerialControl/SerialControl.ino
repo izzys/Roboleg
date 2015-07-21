bdabadb47ef7793adc7694cad4e182e8adaeb974
Drixs <mrkanibl1@gmail.com>
2015-07-21 15:23:47 +0300

    LED on by a button

--- Tests/SerialControl/SerialControl.ino
+++ Tests/SerialControl/SerialControl.ino
@@ -7,51 +7,24 @@ void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
   pinMode(LED,OUTPUT);
-  Timer1.initialize(500000); // every half a second
-  Timer1.pwm(PWM_PORT, 1024); // 1024 equals to 100%; 512 equals to 50% of the duty cycle
-  Timer1.start();
-  //Timer1.attachInterrupt(callback);
+ 
+ 
 }
-
-volatile unsigned long blinkCount = 0; // use volatile for shared variables
 
 void loop() {
-  //analogWrite(PWM_PORT, 255); // The difference between using analogWrite and Timer, is that on analogWrite the period is already default.
-    
-  unsigned long blinkCopy;  // holds a copy of the blinkCount
-
-  // to read a variable which the interrupt code writes, we
-  // must temporarily disable interrupts, to be sure it will
-  // not change while we are reading.  To minimize the time
-  // with interrupts off, just quickly make a copy, and then
-  // use the copy while allowing the interrupt to keep working.
-  noInterrupts();
-  blinkCopy = blinkCount;
-  interrupts();
-
-  Serial.print("blinkCount = ");
-  Serial.println(blinkCopy);
-  delay(1000);
+  if (Serial.available())
+  {
+    char ser = Serial.read();
+    if (ser == 'y')
+    {
+      digitalWrite(LED, HIGH);
+    }
+    if (ser == 'g')
+    {
+      digitalWrite(LED, LOW);
+    }
+  
+  } 
 }
 
-void callback()
-{
-  blinkCount += 1;
-  static bool flag = false;
-  if(flag) // Turn Off
-  {
-    digitalWrite(LED, LOW);
-  }
-  else // Turn On
-  {
-    digitalWrite(LED, HIGH);
-  }
-  flag = !flag;
-//  digitalWrite(LED,HIGH);
-//  Serial.print("On");
-//  delay(1000);
-//  digitalWrite(LED,LOW);
-//  Serial.print("Off");
-//  delay(1000);
-}
 
